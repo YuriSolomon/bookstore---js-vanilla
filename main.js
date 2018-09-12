fc9cd273901579b67bbf39fd614e9c27d40a852c
YuriSolomon <juristein21@gmail.com>
2018-09-12 12:04:43 +0200

    add hover and flip cards

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 getData();
 
-var template;
-
 function getData() {
     fetch("https://api.myjson.com/bins/8zpvs", {})
         .then(response => response.json())
@@ -10,16 +8,30 @@ function getData() {
             var data = realData;
             console.log(data);
 
-            //call books
-            let bookDivs = document.querySelector('#newBook')
-            data.books.forEach(book => {
-                template += `<div>
-                        <a class="fancybox" rel="group" href=${book.detail}><img class="book" src=${book.cover} alt="cover" /></a>
-                    </div>`
-
-                // bookDivs.append(template);
-                document.getElementById("newBook").innerHTML = template;
-            });
-
+            getBooks(data);
         })
+}
+
+function getBooks(data) {
+    var template = "";
+    data.books.forEach(book => {
+        template += `
+                    <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+	                    <div class="flipper">
+		                    <div class="front">
+                                <img class="book" src=${book.cover} alt="cover" /></a>
+		                    </div>
+		                    <div class="back container">
+                                <h3>${book.title}</h3>
+                                <p>${book.description}</p>
+                                <p>Language: ${book.language}</p>
+                                <a class="fancybox" rel="group" href=${book.detail}>
+		                    </div>
+	                    </div>
+                    </div>
+                    `
+
+        document.getElementById("newBook").innerHTML = template;
+    });
+
 }
